86d13e1a5ab35e56604223b24a213f60eaa433f6
root <root@wjzi.local>
2024-06-04 20:24:29 -0400

    Additions and using doubles for the matrix entries

--- Matrix.java
+++ Matrix.java
@@ -1,17 +1,16 @@
 import java.util.stream.IntStream;
 
 public class Matrix {
-    public int m, n;
-    public int[][] matrix;
-    private int[][] transformed;
+    public final int m, n;
+    private final double[][] matrix;// TO NEVER HAVE ITS VALUES CHANGED
 
     public Matrix(int m, int n) {
-	matrix = new int[m][n];
+	matrix = new double[m][n];
 	this.m = m;
 	this.n = n;
     }
 
-    public Matrix(int[][] matrix) {
+    public Matrix(double[][] matrix) {
 	assert matrix.length > 0;
 	this.matrix = matrix;
 	m = matrix.length;
@@ -24,17 +23,17 @@ public class Matrix {
     // transform it once
     public Matrix transform(Matrix other) {
 	assert n == other.m;
-	transformed = new int[m][other.n];
+        double[][] transformed = new double[m][other.n];
 	IntStream.range(0, other.n).parallel().forEach((otherCol) -> {
-		IntStream.range(0, m).parallel().forEach((thisRow) -> {
-			int sum = 0;
-			for (int otherRow = 0; otherRow < n; otherRow++) {
-			    sum += matrix[thisRow][otherRow] *
-				other.matrix[otherRow][otherCol];
-			}
-			transformed[thisRow][otherCol] = sum;
-		    });
+            IntStream.range(0, m).parallel().forEach((thisRow) -> {
+		double sum = 0;
+		for (int otherRow = 0; otherRow < n; otherRow++) {
+		    sum += matrix[thisRow][otherRow] *
+			other.matrix[otherRow][otherCol];
+		}
+		transformed[thisRow][otherCol] = sum;
 	    });
+	});
 	return new Matrix(transformed);
     }
 
@@ -49,11 +48,24 @@ public class Matrix {
 	}
 	return s;
     }
-
+    public double getEntry(int row, int col) {
+        return matrix[row][col];
+    }
+    public static Matrix weightedAvg(Matrix a, Matrix b, double weight) {
+        assert (a.m == b.m);
+	assert (a.n == b.n);
+	double[][] weightedMatrix = new double[a.m][a.n];
+	IntStream.range(0, a.m).parallel().forEach((row) -> {
+            IntStream.range(0, a.n).parallel().forEach((col) -> {
+                weightedMatrix[row][col] = a.matrix[row][col] + ((b.matrix[row][col] - a.matrix[row][col]) * weight);
+	    });
+	});
+	return new Matrix(weightedMatrix);
+    }
     public static void main(String[] args) {
-	Matrix a = new Matrix(new int[][] {{5, 6, 7}, {8, 9, 10}, {11, 12, 13}});
-	Matrix b = new Matrix(new int[][] {{1, 2}, {3, 4}, {5, 6}});
-	Matrix c = new Matrix(new int[][] {{20, 19}, {18, 17}});
+	Matrix a = new Matrix(new double[][] {{5, 6, 7}, {8, 9, 10}, {11, 12, 13}});
+	Matrix b = new Matrix(new double[][] {{1, 2}, {3, 4}, {5, 6}});
+	Matrix c = new Matrix(new double[][] {{20, 19}, {18, 17}});
 	System.out.println(a.transform(b));
 	System.out.println(b.transform(c));
     }
